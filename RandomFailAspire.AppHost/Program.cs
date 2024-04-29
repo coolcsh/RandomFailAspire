495977dec68f90d8ccdf0f8afafcd2853f413971
Scott Hunter <scothu@microsoft.com>
2024-04-29 15:15:23 -0500

    Added external endpoint for the web app

--- RandomFailAspire.AppHost/Program.cs
+++ RandomFailAspire.AppHost/Program.cs
@@ -2,6 +2,6 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 var api = builder.AddProject<Projects.FailApi>("failapi");
 
-builder.AddProject<Projects.RetryApp>("retryapp").WithReference(api);
+builder.AddProject<Projects.RetryApp>("retryapp").WithReference(api).WithExternalHttpEndpoints();
 
 builder.Build().Run();
